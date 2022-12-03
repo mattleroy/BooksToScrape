fbcc56e4ffb3a23b92007c388a99ec862d0169de
Upterry <mattleroy11@gmail.com>
2022-12-02 22:57:12 -0500

    Added Price and In Stock lists

--- main.py
+++ main.py
@@ -15,13 +15,20 @@ soup = BeautifulSoup(page.text, "html.parser")
 cell = soup.find_all(class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
 
 title_list = []
+price_list = []
+in_stock_list = []
+
 page_num = 2
 
 while page_num != 10:
     for item in cell:
         title_list.append(item.h3.a['title'])
+        price_list.append(item.find(class_="price_color").get_text()[1:])
+        in_stock_list.append(item.find(class_='instock availability').get_text().strip())
     url_changer(page_num)
     page_num += 1
 
-
+print(title_list)
+print(price_list)
+print(in_stock_list)
 #print(cell.h3.a['title'])
